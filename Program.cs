f994f5b1a56cb6f21a1a989dc38de7a42191e90e
Leonela Miranda <leonelamiranda141024@gmail.com>
2026-07-29 18:44:57 -0500

    Merge branch 'main' of https://github.com/648leonela/Practicas_CSharp-CONDICIONALES ayuda a comentar

--- Program.cs
+++ Program.cs
@@ -1,3 +1,63 @@
 ﻿// CONDICIONALES
 
 // Ejercicio 1
+// Console.Write("Ingrese un número: ");
+
+// var numero = Console.ReadLine();
+
+// if (String.IsNullOrEmpty(numero))
+// {
+//     Console.WriteLine("No se ingresó ningún número.");
+// }
+// else
+// {
+//     if (int.Parse(numero) % 2 == 0)
+//     {
+//         Console.WriteLine("El número es par.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("El número es impar.");
+//     }
+// }
+
+// Ejercicio 2
+
+// Console.WriteLine("Ingrese Nombre: ");
+
+// var usuario = Console.ReadLine();
+
+// if (String.IsNullOrEmpty(usuario))
+// {
+//     Console.WriteLine("No ingresaste ningun nombre ");
+// }
+// else
+// {
+
+//     Console.WriteLine("El nombre es correcto "+usuario);
+
+// }
+
+
+// Ejercicio 3
+
+// Console.WriteLine("Ingresa tu Edad: ");
+// int edad = int.Parse(Console.ReadLine());
+
+// if (edad < 18 )
+// {
+//     Console.WriteLine("Eres Menor de edad ");
+// }
+// else
+// {
+//     Console.WriteLine("Eres Mayor de edad ");
+// }
+
+// Ejercicio 4
+Console.WriteLine("Ingresa un dia de la semana: ");
+int numero = int.Parse(Console.ReadLine());
+
+switch ()
+
+
+
